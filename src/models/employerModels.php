97b8533b49fffa64cd431ed6da110c47a397478a
Lê Văn Duẩn <93870205+DuanLee7662@users.noreply.github.com>
2023-07-12 17:47:28 +0700

    Update employerModels.php

--- src/models/employerModels.php
+++ src/models/employerModels.php
@@ -66,3 +66,73 @@ class employerModels extends ConnectDB{
             return false;
         }
     }
+
+    // Hàm chọn 1 hàng dữ liệu
+    function selectOne($key,$value){
+        $sql = "SELECT * FROM `employer` WHERE `$key` = '$value'";
+        $user = mysqli_query($this->connection,$sql);
+        // Ép dữ liệu từ mảng về biên
+        $user = mysqli_fetch_array($user);
+        return $user;
+    }
+    // Hàm update
+    function updateOne($id, $name, $email, $address, $description, $contact_info){
+        // Câu truy vấn
+        $sql = "UPDATE
+                    `employer`
+                SET
+                    `name` = '$name',
+                    `email` = '$email',
+                    `description` = '$description',
+                    `contact_info` ='$contact_info',
+                    `address`= '$address'
+
+                WHERE
+                    `id` = '$id'";
+        // Thực hiện truy vấn và kiểm tra
+        mysqli_query($this->connection,$sql);
+        if (mysqli_error($this->connection) == ""){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    function updateAvatar($id, $avatar){
+        // Câu truy vấn
+        $sql = "UPDATE
+                    `employer`
+                SET
+                    `avatar` = '$avatar'
+                WHERE
+                    `id` = '$id'";
+        // Thực hiện truy vấn và kiểm tra
+        mysqli_query($this->connection,$sql);
+        if (mysqli_error($this->connection) == ""){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    //Trả về thông tin các nhà tuyển dụng
+    function selectValues($search, $id){
+        if($id==0){
+            $search_query = "`tname` like '%$search%'";
+        } else {
+            $search_query = "`id` = '$id'";
+        }
+        $sql = "SELECT * FROM `employer` WHERE $search_query ORDER BY `ratesNum` DESC";
+        $tutor= mysqli_query($this->connection,$sql);
+        return $tutor;
+    }
+
+    //Trả về các nhà tuyển dụng theo id
+    function selectById($value){
+        $sql = "SELECT * FROM `employer` WHERE `id` = '$value'";
+        $tutor = mysqli_query($this->connection,$sql);
+        $tutor = mysqli_fetch_array($tutor);
+        return $tutor;
+    }
+
+}

1b9e539df9f364150684c2897a3299c3e50e692c
hason1999 <74525234+hason1999@users.noreply.github.com>
2023-07-13 01:12:29 +0700

    Update recruitment.php

--- src/views/content/employer/recruitment.php
+++ src/views/content/employer/recruitment.php
@@ -1,181 +1,154 @@
-<?php
-class recruitment extends Controllers{
-    //hiển thị danh sách công việc
-    public function jobs_list(){
-        $search = "";
-        if (isset($_GET['search'])){
-            $search = $_GET['search'];
-        }
-        $model = $this->model('recruitmentModels');
-        $jobs = $model->selectValues($search, 0);
-        $model=$this->model('careerModels');
-        $career = $model->getAllValues();
-        $this->view("employer","employer/viewJobs","Việc tìm người",[$search, $jobs, $career]);
-    }
+<link rel="stylesheet" href="<?php echo $actual_link ?>/public/css/header.css">
+<link rel="stylesheet" href="<?php echo $actual_link ?>/public/css/recruitment.css">
 
-    public function job_detail($route = []){
-        $customer_id = "";
-        if(isset($_SESSION['lever']) && $_SESSION['lever'] == 3){
-            $customer_id = $_SESSION['id'];
-        }
+<div style="margin-top:120px;">
+<div style="margin-top:70px;">
+    <div class="main-container">
+        <form class="form" method="post" action="<?php echo $actual_link ?>/recruitment/create_processing">
+            <div class="input-value">
+                <div class="container">
+                    <div class="name-title">
+                        Ngành nghề
+                    </div>
+                    <div></div>
+                    <div class="name-title">
+                        Công việc
+                    </div>
+                </div>
+                <div class="container">
+                    <select class="select-input"  name="career_id" style="font-size: 14px;">
+                        <?php foreach ($data as $career) { ?>
+                            <option value="<?php echo $career['id']?>"><?php echo $career['career_name']?></option>
+                        <?php } ?>
+                    </select>
+                    <div></div>
+                    <input class="select-input" name="job_name" type="text" placeholder="Nhập tên công việc" required>
+                </div>
+            </div>
 
-        $model = $this->model('recruitmentModels');
-        $job = $model->selectByID($route[0]);
-        
-        $count = $model->countApplication($route[0], $customer_id);
-
-        $model=$this->model('careerModels');
-        $career = $model->getAllValues();
-
-        $this->view("employer","employer/jobDetail",$job["job_name"],[$job, $career, $count]);
-    }
-
-    public function company_detail($route = []){
-        $model = $this->model('employerModels');
-        $company = $model->selectOne("id",$route[0]);
-        $model      = $this->model('recruitmentModels');
-        $recruitment       = $model->selectByEmployerId($route[0]);
-        $this->view("employer","employer/viewCompany",$company["name"],[$company, $recruitment]);
-    }
-
-    public function create_recruitment(){
-
-        $actual_link = $this->getUrl();
-        if(isset($_SESSION['lever']) && $_SESSION['lever'] == 2){
-            $model=$this->model('careerModels');
-            $career = $model->getAllValues();
-            $this->view("employer","employer/recruitment","Đăng tin tuyển dụng",$career);
-        } else {
-            $_SESSION['error'] = "Vui lòng đăng nhập!";
-            header("Location: $actual_link/employer/login");
-        }
-     
-    }
-
-    public function search_jobs(){
-
-        $search = "";
-        // Dữ Liệu gửi lên
-        $career_id     = addslashes($_POST['career']);
-        $job_name          = addslashes($_POST['job-name']);
-        $type       = addslashes($_POST['type']);
-        $gender         = addslashes($_POST['gender']);
-        $exp          = addslashes($_POST['exp']);
-        $wage        = addslashes($_POST['wage']);
-        $address       = addslashes($_POST['job-address']);
-
-        // Gọi model
-        $model          = $this->model("recruitmentModels");
-        $jobs = $model->find($career_id, $job_name, $type,  $gender,  $exp, $wage, $address);
-
-        $model=$this->model('careerModels');
-        $career = $model->getAllValues();
-
-        $this->view("user","user/viewJobs","Việc tìm người",[$search, $jobs, $career]);
-    }
-
-    // xử lý đăng tin
-    public function create_processing(){
-        // Dữ Liệu gửi lên
-        $user_id        = $_SESSION['id'];
-        $career_id     = addslashes($_POST['career_id']);
-        $job_name          = addslashes($_POST['job_name']);
-        $type       = addslashes($_POST['type']);
-        $num       = addslashes($_POST['num']);
-        $gender         = addslashes($_POST['gender']);
-        $position   = addslashes($_POST['position']);
-        $exp          = addslashes($_POST['exp']);
-        $wage        = addslashes($_POST['wage']);
-        $deadline        = addslashes($_POST['deadline']);
-        $address       = addslashes($_POST['address']);
-        $description    = addslashes($_POST['description']);
-        $request        = addslashes($_POST['request']);
-        $interest       = addslashes($_POST['interest']);
-
-        // Gọi model
-        $model          = $this->model("recruitmentModels");
-        $actual_link = $this->getUrl();
-        // Lưu dữ liệu và kiểm tra
-        if($model->createValues($user_id, $career_id, $job_name, $type, $num, $gender, $position, $exp, $wage, $deadline, $address, $description, $request, $interest)){
-            header("Location: $actual_link/recruitment/jobs_list");
-        }else{
-            header("Location: $actual_link/recruitment/create_recruitment");
-        }
-    }
-
-    //cập nhận tin tuyển dụng    
-    public function update($route = []){
-        // Dữ Liệu gửi lên
-        $id        = $route[0];
-        $career_id     = addslashes($_POST['career_id']);
-        $job_name          = addslashes($_POST['job_name']);
-        $type       = addslashes($_POST['type']);
-        $num       = addslashes($_POST['num']);
-        $gender         = addslashes($_POST['gender']);
-        $position   = addslashes($_POST['position']);
-        $exp          = addslashes($_POST['exp']);
-        $wage        = addslashes($_POST['wage']);
-        $deadline        = addslashes($_POST['deadline']);
-        $address       = addslashes($_POST['address']);
-        $description    = addslashes($_POST['description']);
-        $request        = addslashes($_POST['request']);
-        $interest       = addslashes($_POST['interest']);
-
-        // Gọi model
-        $model          = $this->model("recruitmentModels");
-        // Lưu dữ liệu và kiểm tra
-        $model->update($id, $career_id, $job_name, $type, $num, $gender, $position, $exp, $wage, $deadline, $address, $description, $request, $interest);
-        $this ->job_detail([$id]); 
-    }
-
-    //gỡ tin đăng tuyển
-    public function job_delete($route = []){
-        $model          = $this->model("recruitmentModels");
-        $model -> job_delete($route[0]);
-
-        $this ->my_recruitment();
-    }
-
-    //ứng tuyển
-    public function apply($route=[]){
-        $actual_link = $this->getUrl();
-        if(isset($_SESSION['lever']) && $_SESSION['lever'] == 3){
+            <div class="input-value">
+                <div class="container2">                
+                    <div class="name-title">
+                        Hình thức
+                    </div>
+                    <div></div>
+                    <div class="name-title">
+                        Số lượng tuyển
+                    </div>
+                    <div></div>
+                    <div class="name-title">
+                        Giới tính yêu cầu
+                    </div>
+                </div>
+                <div class="container2"> 
+                    <select class="select-input" name="type" id="">
+                        <option value="Toàn thời gian">Toàn thời gian</option>
+                        <option value="Bán thời gian">Bán thời gian</option>
+                    </select>
+                    <div></div>
+                    <input name="num" class="select-input" type="number" required>  
+                    <div></div>
+                    <select class="select-input" name="gender" >
+                        <option value="Không">Không</option>
+                        <option value="Nam">Nam</option>
+                        <option value="Nữ">Nữ</option>
+                    </select>      
+                </div>
+            </div>
             
-            $job_id = $route[0];
-            $customer_id = $_SESSION['id'];
+            <div class="input-value">
+                <div class="container3">
+                    <div class="name-title">
+                        Vị trí
+                    </div>
+                    <div></div>
+                    <div class="name-title">
+                        Kinh nghiệm
+                    </div>
+                </div>
+                <div class="container3">
+                    <input class="select-input" name="position" type="text" placeholder="Nhập vị trí tuyển dụng" required>
+                    <div></div>
+                    <select class="select-input" name="exp" id="">
+                        <option value="Không yêu cầu kinh nghiệm">Không yêu cầu kinh nghiệm</option>
+                        <option value="Dưới 1 năm">Dưới 1 năm</option>
+                        <option value="1 năm">1 năm</option>
+                        <option value="2 năm">2 năm</option>
+                        <option value="3 năm">3 năm</option>
+                        <option value="4 năm">4 năm</option>
+                        <option value="5 năm">5 năm</option>
+                        <option value="Trên 5 năm">Trên 5 năm</option>
+                    </select>
+                </div>
+            </div>
 
-            $model    = $this->model("recruitmentModels");
-            $model -> apply($job_id, $customer_id);
-            $this ->job_detail([$job_id]); 
-        } else {
-            $_SESSION['error'] = "Bạn cần đăng nhập tài khoản người tìm việc để ứng tuyển";
-            header("Location: $actual_link/customer/login");
-        }
+            <div class="input-value">
+                <div class="container3">
+                    <div class="name-title">
+                        Mức lương
+                    </div>
+                    <div></div>
+                    <div class="name-title">
+                        Hạn ứng tuyển
+                    </div>
+                </div>
+                <div class="container3">
+                    <input class="select-input" list="browsers" name="wage" type="text" placeholder="Nhập mức lương" required>
+                    <datalist id="browsers">
+                        <option value="Thỏa thuận">
+                        <option value="Dưới 5 triệu">
+                        <option value="5-10 triệu">
+                        <option value="10-15 triệu">
+                        <option value="15-20 triệu">
+                        <option value="20-30 triệu">
+                        <option value="Trên 30 triệu">
+                    </datalist>                    
+                    <div></div>
+                    <input class="select-input"  name="deadline" type="date" request>
+                </div>
+            </div>
+
+            <div class="input-value">
+                <div class="name-title">
+                    Địa điểm làm việc
+                </div>
+                <textarea name="address" placeholder="Nhập địa điểm làm việc" style="height: 70px" request></textarea>
+            </div>
+
+            <div class="input-value">
+                <div class="name-title">
+                    Mô tả công việc
+                </div>
+                <textarea name="description" placeholder="Nhập mô tả công việc"></textarea>
+            </div>
+
+            <div class="input-value">
+                <div class="name-title">
+                    Yêu cầu ứng viên
+                </div>
+                <textarea name="request" placeholder="Nhập yêu cầu cho ứng viên"></textarea>
+            </div>
+
+            <div class="input-value">
+                <div class="name-title">
+                    Quyền lợi
+                </div>
+                <textarea name="interest" placeholder="Nhập quyền lợi của nhân viên"></textarea>
+            </div>
+
+            
+            <div class="input-value">
+                <button type="submit">Đăng tin tuyển dụng</button>
+            </div>
+        </form>
+    </div>
+
+<script>
+  document.querySelectorAll("textarea").forEach(element => {
+    function autoResize(el) {
+      el.style.height = el.scrollHeight + 'px';
     }
-
-     //hủy ứng tuyển
-     public function cancelApply($route = []){
-        $model = $this->model('recruitmentModels');
-        $model -> cancelApplication($_SESSION['id'], $route[0]);
-
-        $job_id = $route[0];
-        $this ->job_detail([$job_id]); 
-    }
-
-    public function apply_delete($route = []){
-        $model          = $this->model("recruitmentModels");
-        $model -> cancelApplication($route[0], $route[1]);
-
-        $this ->my_recruitment();
-    }
-
-    public function my_recruitment(){
-
-        $model          = $this->model('recruitmentModels');
-        $jobs           = $model->selectByEmployerId($_SESSION['id']);
-        $applications   = $model->selectApplication($_SESSION['id'], null);
-
-        $this->view("employer","employer/myRecruitment","Tin tuyển dụng của công ty",[$jobs, $applications]);
-    }
-
-}
+    autoResize(element);
+    element.addEventListener('input', () => autoResize(element));
+  });
+</script>
